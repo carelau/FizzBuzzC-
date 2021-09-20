473bc0b92aeb03519720ca6b94546cd7bf4e0809
carelau <carelau2812@gmail.com>
2021-09-20 16:57:34 -0400

    added bang bong fezz

--- Program.cs
+++ Program.cs
@@ -4,24 +4,55 @@ namespace FizzBuzz
 {
     class Program
     {
-        static void Main(string[] args)
+        static string Fizz (int num, string output){
+            if (num % 3 == 0){
+                output += "Fizz";
+                }
+                return output;
+            }
+            static string Fezz (int num, string output){
+            if (num % 13 == 0){
+                output += "Fezz";
+                }
+                return output;
+            }
+
+        static string Buzz (int num, string output){
+            if (num % 5 == 0){
+                output += "Buzz";
+            }
+                return output;
+            }  
+
+         static string Bang (int num, string output){
+            if (num % 7 == 0){
+                output += "Bang";
+            }
+                return output;
+            }    
+        static string Bong (int num, string output){
+            if (num % 11 == 0 && num % 13 ==0){
+                output += "Bong";
+            } else if (num % 11 ==0){
+                output = "Bong";
+            }
+                return output;
+            }                 
+      
+        static void Main (string[] args)
         {
-            for(int i=1; i <=100; i++)
+            for(int i=1; i <=195; i++)
             {
-                if(i %3 == 0 && i % 5 == 0) {
-                    Console.WriteLine("FizzBuzz");
-                }
-                else if (i %3 == 0){
-                    Console.WriteLine("Fizz");
-                }
-                else if (i % 5 == 0) {
-                    Console.WriteLine("Buzz");
-                }
-                else {
-                  Console.WriteLine(i);  
-                }
-                
+
+                string output = "";
+                output = Fizz (i, output);
+                output = Fezz (i, output);
+                output = Buzz (i, output);
+                output = Bang (i, output);
+                output = Bong (i, output);
+                Console.WriteLine (output == "" ? i : output);
             }
         }
     }
 }
+
